0c4bb0d90c713511716b4ac101bedab4817f6343
Pedro Júnior <pedrojdev@gmail.com>
2024-03-27 08:34:01 -0300

    feat: add booking to attendee when booking is made

--- src/Eventify.Application/Bookings/Events/BookingPlacedHandler.cs
+++ src/Eventify.Application/Bookings/Events/BookingPlacedHandler.cs
@@ -1,3 +1,4 @@
+using Eventify.Domain.Attendees.Repository;
 using Eventify.Domain.Bookings;
 using Eventify.Domain.Bookings.Enums;
 using Eventify.Domain.Bookings.Events;
@@ -7,15 +8,35 @@ using Microsoft.Extensions.Logging;
 namespace Eventify.Application.Bookings.Events;
 
 internal sealed class BookingPlacedHandler(
+    IAttendeeRepository attendeeRepository,
     IBookingRepository bookingRepository,
     ILogger<BookingPlacedHandler> logger
 ) : IDomainEventHandler<BookingPlaced>
 {
-    public Task Handle(BookingPlaced e, CancellationToken cancellationToken)
+    public async Task Handle(BookingPlaced e, CancellationToken cancellationToken)
     {
         logger.LogInformation("Handling {Event} domain event", nameof(BookingPlaced));
 
-        return ProcessBookingPaymentAsync(e.Booking, cancellationToken);
+        await AddBookingToAttendeeAsync(e.Booking, cancellationToken);
+        await ProcessBookingPaymentAsync(e.Booking, cancellationToken);
+    }
+    
+    private async Task AddBookingToAttendeeAsync(Booking booking, CancellationToken cancellationToken)
+    {
+        var attendee = await attendeeRepository.GetAsync(booking.AttendeeId, cancellationToken);
+        if (attendee is null)
+        {
+            throw new ApplicationException($"Attendee {booking.AttendeeId} not found");
+        }
+
+        var result = await attendee.AddBooking(booking)
+            .ThenAsync(_ => attendeeRepository.UpdateAsync(attendee, cancellationToken));
+
+        if (result.IsError)
+        {
+            throw new ApplicationException($"Failed to add booking {booking.Id} to " +
+                                           $"attendee {booking.AttendeeId}: {result.FirstError.Description}");
+        }
     }
 
     private async Task ProcessBookingPaymentAsync(Booking booking, CancellationToken cancellationToken)
--- tests/Eventify.Application.UnitTests/Bookings/Events/BookingPlacedHandlerTests.cs
+++ tests/Eventify.Application.UnitTests/Bookings/Events/BookingPlacedHandlerTests.cs
@@ -1,4 +1,6 @@
 using Eventify.Application.Bookings.Events;
+using Eventify.Domain.Attendees;
+using Eventify.Domain.Attendees.Repository;
 using Eventify.Domain.Bookings;
 using Eventify.Domain.Bookings.Events;
 using Eventify.Domain.Bookings.Repository;
@@ -9,6 +11,7 @@ namespace Eventify.Application.UnitTests.Bookings.Events;
 [TestSubject(typeof(BookingPlacedHandler))]
 public sealed class BookingPlacedHandlerTests
 {
+    private readonly IAttendeeRepository attendeeRepository = A.Fake<IAttendeeRepository>();
     private readonly IBookingRepository bookingRepository = A.Fake<IBookingRepository>();
     private readonly ILogger<BookingPlacedHandler> logger = A.Fake<ILogger<BookingPlacedHandler>>();
 
@@ -16,18 +19,64 @@ public sealed class BookingPlacedHandlerTests
 
     private readonly BookingPlacedHandler sut;
 
+    private readonly Attendee attendee = Factories.Attendee.CreateAttendee();
     private readonly Booking booking = Factories.Booking.CreateBooking();
 
     private readonly BookingPlaced e;
 
     public BookingPlacedHandlerTests()
     {
-        sut = new BookingPlacedHandler(bookingRepository, logger);
+        sut = new BookingPlacedHandler(attendeeRepository, bookingRepository, logger);
 
         e = new BookingPlaced(booking);
+        
+        A.CallTo(() => attendeeRepository.GetAsync(booking.AttendeeId, cancellationToken))
+            .Returns(attendee);
 
         A.CallTo(() => bookingRepository.GetAsync(e.Booking.Id, cancellationToken))
             .Returns(booking);
+    }
+    
+    [Fact]
+    public async Task Handle_WhenCalled_ShouldAddBookingToAttendee()
+    {
+        // Arrange
+        // Act
+        await sut.Handle(e, cancellationToken);
+
+        // Assert
+        A.CallTo(() => attendeeRepository.UpdateAsync(
+                A<Attendee>.That.Matches(a => a.BookingIds.Contains(booking.Id)), cancellationToken))
+            .MustHaveHappenedOnceExactly();
+    }
+
+    [Fact]
+    public async Task Handle_WhenAttendeeDoesNotExist_ShouldThrowApplicationException()
+    {
+        // Arrange
+        A.CallTo(() => attendeeRepository.GetAsync(booking.AttendeeId, cancellationToken))
+            .Returns(null as Attendee);
+
+        // Act
+        var act = () => sut.Handle(e, cancellationToken);
+
+        // Assert
+        await act.Should().ThrowExactlyAsync<ApplicationException>()
+            .WithMessage($"Attendee {booking.AttendeeId} not found");
+    }
+
+    [Fact]
+    public async Task Handle_WhenAddBookingToAttendeeFails_ShouldThrowApplicationException()
+    {
+        // Arrange
+        attendee.AddBooking(booking);
+
+        // Act
+        var act = () => sut.Handle(e, cancellationToken);
+
+        // Assert
+        await act.Should().ThrowExactlyAsync<ApplicationException>()
+            .WithMessage($"Failed to add booking {booking.Id} to attendee {booking.AttendeeId}: *");
     }
     
     [Fact]
